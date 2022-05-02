8acc1d895559faddf866616598d43930ef467a71
Derrick Schwabe <drschwabe@fastmail.com>
2022-05-01 20:33:29 -0600

    Improve 'is' mod to accommodate for a plain string so param doesn't have to be a fn

--- app/mods/is.js
+++ app/mods/is.js
@@ -2,8 +2,15 @@ const {html} = require('uhtml')
 const _ = require('underscore')
 
 const is = (condition, template, elseTemplate) => {
-  if(condition) return template()
-  if(elseTemplate) return elseTemplate()
+  if(condition) {
+    log(template)
+    if(_.isString(template)) return () => template
+    return template() 
+  }
+  if(elseTemplate) {
+    if(_.isString(elseTemplate)) return () => elseTemplate
+    return elseTemplate() 
+  }
 }
 
 module.exports = is
