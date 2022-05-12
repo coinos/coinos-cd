8eb52e27b02cf1f2f58e26418172a7b32676631f
Derrick Schwabe <drschwabe@fastmail.com>
2022-05-12 16:10:41 -0600

    Revert "New utility endpoint for deleting a doc of a deploy (ie- abandoned db doc; w/ no longer existing server)"
    This reverts commit 624adc370c39e331ebc6965d9c47a49251d079eb.

--- app/server.js
+++ app/server.js
@@ -147,18 +147,6 @@ exApp.post('/deploy/:deployId/is-online', async (req, res) => {
   return res.sendStatus(200)
 })
 
-exApp.post('/deploy/:deployId/delete-doc', async (req, res) => {
-  try {
-    const deploy = await deploysDb.get(req.params.deployId)
-    const removed = await deploysDb.remove(deploy)
-    log(removed)
-    res.sendStatus(200)
-  } catch (e) {
-    log(e) 
-    res.sendStatus(500)
-  }
-})
-
 exApp.post('/deploy/:deployId/destroy', async (req, res) => {
 
   const deploy = await deploysDb.get(req.params.deployId)
